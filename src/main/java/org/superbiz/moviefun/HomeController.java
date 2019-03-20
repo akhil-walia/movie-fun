bb86784abdcf03119a22026ca34a9662c2270a23
apgovinda <apgovinda@gmail.com>
2019-03-20 13:53:32 +0530

    commit

--- src/main/java/org/superbiz/moviefun/HomeController.java
+++ src/main/java/org/superbiz/moviefun/HomeController.java
@@ -35,14 +35,3 @@ public class HomeController {
         return "index";
     }
 }
-/*
-
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
-    }
-
-
-
-*/
